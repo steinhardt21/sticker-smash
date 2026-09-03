d75e83ad10d9fc2dc9ff0ab95e0e820f45637399
steinhardt21 <alexandru.enache@protonmail.com>
2026-09-03 16:51:38 +0300

    created button and imageviewer

---
+++ src/components/Button.tsx
@@ -0,0 +1,39 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+type Props = {
+  label: string;
+};
+
+export default function Button({ label }: Props) {
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable style={styles.button} onPress={() => alert('Button pressed!')}>
+        <Text style={styles.buttonText}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
